8a9821f023c94bd7e9571b4032969f9a364375a2
dosumbaeva111 <dosumbaeva111@gmail.com>
2021-04-01 19:00:10 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(method(21, 30));
+        System.out.println(method(15, 18));
+        System.out.println(method(14, 30));
+        System.out.println(method(45, 40));
+        System.out.println(method(10, 15));
+
+    }
+
+    public static String method(int age, int temperature) {
+        String result = "";
+        if (age > 20 && age < 45) {
+            if (temperature < -20 && temperature > 30) {
+
+                result = "Можно идти гулять!";
+            }
+
+        } else if (age < 20 && temperature > 0 && temperature < 28) {
+            result = "Можно идти гулять!!!";
+        }
+        else if (age >= 45 && temperature > -10 && temperature < 25) {
+            result = "Можно идти гулять!!!";
+        } else {
+            result = "Лучше остаться дома!";
+        }
+        return result;
+
+    }
+
+}
